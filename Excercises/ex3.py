a53f49a2bd2c4ae6990d2f3d03449fbbddf93b1b
Marisa Janae <marisa.briggs.nyc@gmail.com>
2017-09-06 14:28:21 -0400

    Updated Excercise 3

--- Excercises/ex3.py
+++ Excercises/ex3.py
@@ -1,4 +1,4 @@
-#Assignment: Excercise 3: Numbers and Math 
+#lsAssignment: Excercise 3: Numbers and Math 
 print("I will now count my chickens: ")
 print("Hens", 25 + 30 / 6)
 print("Roosters", 100 - 25 * 3 % 4)
