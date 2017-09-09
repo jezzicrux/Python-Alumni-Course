f0c47d09af68bf88eedb3d3ae128c652beeda821
Jessie <jessicamc514@gmail.com>
2017-09-09 10:13:30 -0400

    Adding new files

--- Excercises/HW1.py
+++ Excercises/HW1.py
@@ -1,13 +1,24 @@
+#a message stating it going to count the number on chickens
 print("I will now count my chickens:")
+#number of hens
 print("Hens", 25 + 30 / 6)
+#number of roosters
 print("Roosters", 100 - 25 * 3 % 4)
+#a message counting the number of eggs
 print("Now I will count the eggs:")
+#The math for calculating the number of eggs
 print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+#A message
 print("Is it true that 3 + 2 < 5 - 7?")
+#the answer
 print(3 + 2 < 5 - 7)
+#A question and then the answer. Answer is found with a math equation
 print("What is 3 + 2?", 3 + 2)
+#A question and then the answer. Answer is found with a math equation
 print("What is 5 - 7?", 5 - 7)
+#a message
 print("Oh, that's why it's False.")
+#
 print("How about some more.")
 print("Is it greater?", 5 > -2)
 print("Is it greater or equal?", 5 >= -2)
---
+++ Excercises/HW2.py
@@ -0,0 +1,10 @@
+#Getting the average vaules on each number set
+average_number_set1=(44+64+88+53+89)/5
+average_number_set2=(39+45+55+90+95+96)/6
+average_number_set3=(54+(-45)+(-10)+90)/4
+average_number_set4=(55+65+75+95+32)/5
+#Displaying the average along with a statement
+print("The average of 5 given numbers is:", average_number_set1)
+print("The average of 6 given numbers is:", average_number_set2)
+print("The average of 4 given numbers is:", average_number_set3)
+print("The average of 5 given numbers is:", average_number_set4)
