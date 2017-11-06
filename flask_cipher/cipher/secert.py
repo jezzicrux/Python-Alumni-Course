bb9db4dbc0e17ebcf99cf0437754f72ac71816e7
jezzicrux <jessicamc514@gmail.com>
2017-11-06 11:29:05 -0500

    flask cipher

---
+++ flask_cipher/cipher/secert.py
@@ -0,0 +1,38 @@
+## FROM THE CURRENT DIRECTORY, IMPORT APP
+from . import app
+from flask import render_template, request
+
+@app.route('/')
+def index():
+	return render_template('/index.html')
+
+@app.route('/cipher', methods = ['GET', 'POST'])
+def secertcode():
+	if request.method == 'POST':
+		message = request.form['message']
+		number = request.form['key']
+		code = list(str.lower(message))
+		key = int(number)
+		if key < 27:
+			letters = list('abcdefghijklmnopqrstuvwxyz')
+			new_message=code
+			for q in range (0,len(code)):
+				i = letters.index(code[q])
+				if new_message[q] in letters:
+					if code[q] == letters[i]:
+						new_index=i+key
+						new_message[q] = letters[new_index]
+					else:
+						pass
+				else:
+					pass
+			cipher = "".join(new_message)
+			print(cipher)
+			return render_template('/cipher.html', decrypt = cipher)
+		else:
+			warning = "WRONG KEY"
+			return render_template('/index.html')
+	else:
+		print('get method')
+		return render_template('/index.html')
+	return render_template('/cipher.html', decrypt = cipher)
